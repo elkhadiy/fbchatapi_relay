4230ed2b54c557341a1b6cf1ad13e7624b2bb15f
Yassine El Khadiri <yassineelkhadiri@gmail.com>
2018-10-03 16:54:43 +0200

    add login scripts

---
+++ scripts/login.ps1
@@ -0,0 +1,26 @@
+#Requires -Version 3
+
+$email = Read-host "Email"
+$pw = Read-host -AsSecureString "Password"
+$pw = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($pw))
+
+$body = @{
+  "email"="$email"
+  "password"="$pw"
+}
+
+$header = @{
+  "Accept"="application/json"
+  "Content-Type"="application/json"
+}
+
+$token = Invoke-RestMethod -Uri "https://messenger4business.elkrafts.xyz/login" -Method 'Post' -Body $(ConvertTo-Json $body) -Headers $header
+
+$header = @{
+  "Accept"="application/json"
+  "Authorization"=$token.authorization
+}
+
+$headerFile = ".session_header_" + $email + ".xml"
+
+$header | Export-CliXml $headerFile
---
+++ scripts/login.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function getJsonVal () {
+    python -c "import sys, json; obj=json.load(sys.stdin); print(obj['$1']);";
+}
+
+echo -n "Email: "
+read email
+echo -n "Password: "
+read -s password
+echo
+
+res=$( \
+curl -s --request POST \
+    -H "Content-Type: application/json" \
+    -d '{"email":"'$email'","password":"'$password'"}' \
+    https://messenger4business.elkrafts.xyz/login \
+    )
+
+token=$(echo $res | getJsonVal authorization)
+
+echo "Accept: application/json" > .session_header_$email
+echo "Authorization: "$token >> .session_header_$email
